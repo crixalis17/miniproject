ee2e596ecb2d3b4f56a62ae5d8e825a0ef52fdfb
nixonakash01 <akash.nixon@testsigma.com>
2022-10-26 12:23:00 +0530

    added table and updated functionality in frontend

--- davion/src/app/app.component.ts
+++ davion/src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
+import {MatDialog} from "@angular/material/dialog";
 
 
 @Component({
@@ -11,5 +12,15 @@ import { MatInputModule } from '@angular/material/input';
 export class AppComponent {
   title = 'app';
   value = '';
+  name1 = 'Ramesh';
+    no_of_tweets = 10;
+    bullying = 5;
+    percentage_bully = 50.00;
+  show: boolean=false;
   onEnter(value: string) { this.value = value; }
+  toggle() {
+    this.show = !this.show;
+  }
+
 }
+
