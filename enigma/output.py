cc7e2619b3a13324028f973a32d75934d6a6fcf7
Rakesh Elango <e.rakesh176@gmail.com>
2022-10-28 11:21:52 +0530

    refactor code

--- enigma/output.py
+++ enigma/output.py
@@ -8,28 +8,29 @@ os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
 from transformers import BertTokenizerFast
 from transformers import TFBertModel
 from bert_tokenizer import tokenize
+def final_output():
+  df = pd.read_csv(r'C:\Users\Asus\Documents\miniproject\files\output_files\tweets_after_preprocessing.csv')
+  input_values = df['text_clean'].values
 
-df = pd.read_csv(r'C:\Users\Asus\Documents\miniproject\files\output_files\tweets_after_preprocessing.csv')
-input_values = df['text_clean'].values
+  MAX_LEN=128
+  
+  print("LOADING BERT MODEL")
+  new_model = tf.keras.models.load_model(r'C:\Users\Asus\Documents\miniproject\enigma\my_model')
 
-MAX_LEN=128
-#input_ids, attention_masks = tokenize(input_values, MAX_LEN)
-print("LOADING BERT MODEL")
-new_model = tf.keras.models.load_model(r'C:\Users\Asus\Documents\miniproject\enigma\my_model')
-example_input = ['You are the worst person i have ever seen',
-                'you seem normal',
-                'I really like the way you speak']
 
-input_ids, attention_masks = tokenize(input_values, MAX_LEN)
-result_bert = new_model.predict([input_ids, attention_masks])
-print("SCORE : \n")
-for j in range(len(result_bert)):
-  print(max(result_bert[j])*100)
-print('\n')
-oplabels = ['Bullying','Not Bullying']
+  input_ids, attention_masks = tokenize(input_values, MAX_LEN)
+  result_bert = new_model.predict([input_ids, attention_masks])
+  
+  print("SCORE : \n")
+  for j in range(len(result_bert)):
+    print(max(result_bert[j])*100)
+  print('\n')
+  oplabels = ['Bullying','Not Bullying']
 
-y_pred_bert =  np.zeros_like(result_bert)
-y_pred_bert[np.arange(len(y_pred_bert)), result_bert.argmax(1)] = 1
-#print(y_pred_bert)
-for i in y_pred_bert.argmax(1):
-  print(oplabels[i])
+  y_pred_bert =  np.zeros_like(result_bert)
+  y_pred_bert[np.arange(len(y_pred_bert)), result_bert.argmax(1)] = 1
+  #print(y_pred_bert)
+  for i in y_pred_bert.argmax(1):
+    print(oplabels[i])
+
+    return 0
--- rylai/scrape.py
+++ rylai/scrape.py
@@ -7,37 +7,45 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
+import numpy as np
+import pandas as pd
 
-PATH = "C:\chromedriver.exe"
-options = webdriver.ChromeOptions()
-#username = 
-#password =     
-driver = webdriver.Chrome(service=Service("C:\\chromedriver.exe"),options =options)
-driver.get("https://twitter.com/RAMESH_ROKETMAN")
 
-#time.sleep(10)
-profile_pic_click = WebDriverWait(driver, 20).until(EC.visibility_of_element_located((By.XPATH,"//*[@id='react-root']/div/div/div[2]/main/div/div/div/div/div/div[3]/div/div/div/div/div[1]/div[1]/div[2]/div/div[2]/div/a/div[4]/div"))).click()
+def webscrape(path):
+    path = "C:\chromedriver.exe"
+    options = webdriver.ChromeOptions()
+  
+    driver = webdriver.Chrome(service=Service(path),options =options)
+    driver.get("https://twitter.com/RAMESH_ROKETMAN")
 
-profile_pic = driver.find_element(By.XPATH,"//img[contains(@class,'css-9pa8cd')]").get_attribute('src')
-urllib.request.urlretrieve(str(profile_pic),r"C:\Users\Asus\Documents\miniproject\files\output_files\profile_pic.jpg")
+    
+    profile_pic_click = WebDriverWait(driver, 20).until(EC.visibility_of_element_located((By.XPATH,"//*[@id='react-root']/div/div/div[2]/main/div/div/div/div/div/div[3]/div/div/div/div/div[1]/div[1]/div[2]/div/div[2]/div/a/div[4]/div"))).click()
 
-close_button_click = WebDriverWait(driver, 20).until(EC.visibility_of_element_located((By.XPATH,"//div[@role='button']"))).click()
+    profile_pic = driver.find_element(By.XPATH,"//img[contains(@class,'css-9pa8cd')]").get_attribute('src')
+    urllib.request.urlretrieve(str(profile_pic),r"C:\Users\Asus\Documents\miniproject\files\output_files\profile_pic.jpg")
 
-profile_name = WebDriverWait(driver, 20).until(EC.visibility_of_element_located((By.XPATH,"//*[@id='react-root']/div/div/div[2]/main/div/div/div/div/div/div[1]/div[1]/div/div/div/div/div/div[2]/div/h2/div/div/div/div/span[1]/span/span")))
-all_tweets =set()
-time.sleep(2)
-tweets = WebDriverWait(driver, 5).until(EC.visibility_of_all_elements_located((By.XPATH,"//div[@data-testid='tweetText']")))
-for tweet in tweets:
-    all_tweets.add(tweet.text)
-driver.execute_script('window.scrollTo(0,document.body.scrollHeight);')
-time.sleep(2)
-tweets = WebDriverWait(driver, 5).until(EC.visibility_of_all_elements_located((By.XPATH,"//div[@data-testid='tweetText']")))
-for tweet in tweets:
-    all_tweets.add(tweet.text)
+    close_button_click = WebDriverWait(driver, 20).until(EC.visibility_of_element_located((By.XPATH,"//div[@role='button']"))).click()
 
-print(profile_name.text)
+    profile_name = WebDriverWait(driver, 20).until(EC.visibility_of_element_located((By.XPATH,"//*[@id='react-root']/div/div/div[2]/main/div/div/div/div/div/div[1]/div[1]/div/div/div/div/div/div[2]/div/h2/div/div/div/div/span[1]/span/span")))
+    all_tweets =set()
+    time.sleep(2)
+    tweets = WebDriverWait(driver, 5).until(EC.visibility_of_all_elements_located((By.XPATH,"//div[@data-testid='tweetText']")))
+    for tweet in tweets:
+        all_tweets.add(tweet.text)
+    driver.execute_script('window.scrollTo(0,document.body.scrollHeight);')
+    time.sleep(2)
+    tweets = WebDriverWait(driver, 5).until(EC.visibility_of_all_elements_located((By.XPATH,"//div[@data-testid='tweetText']")))
+    for tweet in tweets:
+        all_tweets.add(tweet.text)
 
-print(all_tweets,len(all_tweets))
+    print(profile_name.text)
+
+    #print(all_tweets,len(all_tweets))
+
+    df = pd.DataFrame(all_tweets,columns=["OriginalTweets"])
+    df.to_csv(r'C:\Users\Asus\Documents\miniproject\files\input_files\tweets_after_scraping.csv')
+
+    return 0
 
 
 
